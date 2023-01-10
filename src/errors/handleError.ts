f8be648a34078082722427b3245f7eb28efcc318
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-10 10:08:04 -0300

    add project structure

---
+++ src/errors/handleError.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./AppError";
+
+
+const handleError = async (error: Error, request: Request, response: Response, next: NextFunction) => {
+
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({message: error.message})
+    }
+
+    console.log(error.message, error.name, "Aqui")
+
+    return response.status(500).json({message: "Internal server error"})
+}
+
+export default handleError
