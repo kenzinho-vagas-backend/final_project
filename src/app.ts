88c5598344aa5f144cb6d1a8b63f93913290b6bb
netomilfont <netomilfont@hotmail.com>
2023-01-10 23:08:09 +0100

    feat: add routes off companies

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
 import handleError from './errors/handleError'
+import { companiesRoutes } from './router/companies.routes'
 
 const app = express()
 app.use(express.json())
+
+app.use('/companies', companiesRoutes)
 
 app.use(handleError)
 
---
+++ src/router/companies.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const companiesRoutes = Router()
+
+companiesRoutes.post('')
+companiesRoutes.get('')
+companiesRoutes.patch('')
+companiesRoutes.delete('')
+
+export { companiesRoutes }
