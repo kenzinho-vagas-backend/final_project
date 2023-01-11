68d053f5e0728c9d00b4603ac8f271732284f12a
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-11 15:59:50 -0300

    feat/jobPost: add import and export from ensuranceIsUser middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { ensurePatchDataIsValidMiddleware } from './validateSchema.middleware'
 import { ensureUserExists } from './ensureUserExists.middleware'
 import {ensuranceUserIsAdm} from './ensuranceUserIsAdm.middleware'
+import {ensuranceIsUserExists} from './ensuranceIsUserExists.middleware'
 
-export { ensurePatchDataIsValidMiddleware, ensureUserExists, ensuranceUserIsAdm }
+export { ensurePatchDataIsValidMiddleware, ensureUserExists, ensuranceUserIsAdm, ensuranceIsUserExists }
