de9ebd08bdd5070d4201a335c16494495ee29f3b
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-11 12:29:06 -0300

    feat: delete userJob route added

--- src/controllers/jobToUser/jobToUser.controller.ts
+++ src/controllers/jobToUser/jobToUser.controller.ts
@@ -1,6 +1,7 @@
 import { Request, response, Response } from "express";
 import createJobToUserService from "../../services/jobToUser/createJobUser.service";
-import listJobToUserService from "../../services/jobToUser/listJobToUser.service"; 
+import listJobToUserService from "../../services/jobToUser/listJobToUser.service";
+import deleteJobToUserService from "../../services/jobToUser/deleteJobToUser.service"; 
 
 
 export const createJobToUserController = async (req: Request, res: Response) => {
@@ -12,5 +13,13 @@ export const createJobToUserController = async (req: Request, res: Response) => 
 
 export const listJobToUserController = async (req: Request, res: Response) => {
     const response = await listJobToUserService()
+
     return res.status(200).json(response)
+}
+
+export const deleteJobToUserController = async (req: Request, res: Response) => {
+    const jobId: string = req.params.id
+    const response = await deleteJobToUserService(jobId)
+
+    return res.status(400).json(response)
 }
--- src/router/jobUser.routes.ts
+++ src/router/jobUser.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 // import { createJobToUserController } from "../controllers";
 import { createJobToUserController } from "../controllers/jobToUser/jobToUser.controller";
 import { listJobToUserController } from "../controllers/jobToUser/jobToUser.controller";
+import { deleteJobToUserController } from "../controllers/jobToUser/jobToUser.controller";
 
 const jobUserRoutes = Router()
 
 jobUserRoutes.post('', createJobToUserController)
 jobUserRoutes.get('', listJobToUserController)
+jobUserRoutes.get('/:id', deleteJobToUserController)
 
 export default jobUserRoutes
