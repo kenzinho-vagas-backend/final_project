3e112f11e336eb2232bad507f5fb5915eaf3d91b
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-11 11:55:10 -0300

    feat: get userJob route added

--- src/controllers/jobToUser/jobToUser.controller.ts
+++ src/controllers/jobToUser/jobToUser.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import createJobToUserService from "../../services/jobToUser/createJobUser.service";
+import listJobToUserService from "../../services/jobToUser/listJobToUser.service"; 
 
 
 export const createJobToUserController = async (req: Request, res: Response) => {
@@ -7,4 +8,9 @@ export const createJobToUserController = async (req: Request, res: Response) => 
     const response = await createJobToUserService(jobUser)
 
     return res.status(201).json(response)
+}
+
+export const listJobToUserController = async (req: Request, res: Response) => {
+    const response = await listJobToUserService()
+    return res.status(200).json(response)
 }
--- src/router/jobUser.routes.ts
+++ src/router/jobUser.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createJobToUserController } from "../controllers";
+// import { createJobToUserController } from "../controllers";
+import { createJobToUserController } from "../controllers/jobToUser/jobToUser.controller";
+import { listJobToUserController } from "../controllers/jobToUser/jobToUser.controller";
 
 const jobUserRoutes = Router()
 
 jobUserRoutes.post('', createJobToUserController)
+jobUserRoutes.get('', listJobToUserController)
 
 export default jobUserRoutes
