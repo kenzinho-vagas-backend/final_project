3c4a549ca8ae40e172c1e52dd66dd07482d37ad7
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-11 09:20:53 -0300

    fix: merge adjustment

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
 import handleError from './errors/handleError'
-
 import companiesRoutes from './router/companies.routes'
+import { jobRoutes } from './router'
 
 
 const app = express()
 app.use(express.json())
 
-
+app.use('/jobs', jobRoutes)
 app.use('/companies', companiesRoutes)
 
 app.use(handleError)
