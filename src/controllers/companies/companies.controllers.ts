3c6f45cd5a531ba5d6a2b4bb4edd3c63317bc7b3
netomilfont <netomilfont@hotmail.com>
2023-01-11 02:44:55 +0100

    feat: add service of delete company

--- src/controllers/companies/companies.controllers.ts
+++ src/controllers/companies/companies.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { ICompanyRequest } from '../../interfaces/company.interface';
 import { createCompanyService } from '../../services/companies/createCompany.service';
+import { deleteCompanyService } from '../../services/companies/deleteCompany.service';
 import { listAllCompaniesService } from '../../services/companies/listAllCompanies.service';
 import { uptadeCompanyService } from '../../services/companies/uptadeCompany.service';
 
@@ -23,7 +24,9 @@ const uptadeCompanyController = async (req: Request, res: Response) => {
 }
 
 const deleteCompanyController = async (req: Request, res: Response) => {
-    
+    const companyId: string = req.params.id
+    await deleteCompanyService(companyId)
+    return res.status(200).send()
 }
 
-export { createCompanyController, listAllCompaniesController, uptadeCompanyController }
+export { createCompanyController, listAllCompaniesController, uptadeCompanyController, deleteCompanyController }
