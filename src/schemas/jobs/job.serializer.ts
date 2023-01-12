946b5471e4b0b59fd33d6ce16dbcbb97d4169c30
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-12 10:57:00 -0300

    feat/jobPost: add interface

--- src/schemas/jobs/job.serializer.ts
+++ src/schemas/jobs/job.serializer.ts
@@ -1,10 +1,20 @@
 import * as yup from 'yup'
+import { SchemaOf } from 'yup'
+import { IJobRequest, IJobResponse } from '../../interfaces/job.interface'
 
-export const createJobSerializer = yup.object().shape({
+export const createJobSerializer: SchemaOf<IJobRequest>= yup.object().shape({
     wage: yup.string().required('Wage is a required field'),
     modality: yup.string().required('Modality is a required field'),
     jobLevel: yup.string().required('Job level is a required field'),
     jobUrl: yup.string().required('Job url is a required field'),
-    companies: yup.string().uuid(),
-    technologies: yup.string().uuid()
 })
+
+export const returnJobSerializer: SchemaOf<IJobResponse>= yup.object().shape({
+    wage: yup.string(),
+    modality: yup.string(),
+    jobLevel: yup.string(),
+    jobUrl: yup.string(),
+    companiesId: yup.string(),
+    createdAt: yup.date()
+})
+
