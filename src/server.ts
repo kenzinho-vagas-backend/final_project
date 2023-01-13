460654e498bde4408171a137eb30b877eade57ef
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-13 11:05:40 -0300

    fix: update data-source

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import app from './app'
 import AppDataSource from './data-source'
 
-(async () => {
-
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error('Error during Data Source initialization', err)
-    })
-    
+AppDataSource.initialize().then(() => {
+    console.log('Database connected!')
     app.listen(3000, () => {
-        console.log('Server running')
-    })    
-})()
+        console.log('Server running in port 3000')
+    })
+}).catch(err => {
+    console.log(err)
+})
