c5a5a7dd9ce7cabdd7556646486cf4b70303f6b7
Gabriela <gabrielacamarchiori@gmail.com>
2023-01-16 16:58:38 -0300

    feat/jobPost: fix deleteJobUser service and update jobUser routes

--- src/router/jobs/jobUser.routes.ts
+++ src/router/jobs/jobUser.routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import { createJobToUserController } from '../../controllers/jobToUser/jobToUser.controller'
 import { listJobToUserController } from '../../controllers/jobToUser/jobToUser.controller'
 import { deleteJobToUserController } from '../../controllers/jobToUser/jobToUser.controller'
+import { ensuranceIsOwnerJobMiddleware } from '../../middlewares/ensuranceIsOwnerJob.middleware'
 import { ensureAuthMiddleware } from '../../middlewares/ensureAuth.middleware'
 
 const jobUserRoutes = Router()
 
 jobUserRoutes.post('', ensureAuthMiddleware,createJobToUserController)
-jobUserRoutes.get('', ensureAuthMiddleware, listJobToUserController)
+jobUserRoutes.get('', ensureAuthMiddleware,ensuranceIsOwnerJobMiddleware ,listJobToUserController)
 jobUserRoutes.delete('/:id', ensureAuthMiddleware,deleteJobToUserController)
 
 export default jobUserRoutes
