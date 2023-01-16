7550ee24330325261100470be9d50c0aa968fe24
Natalia <natpereira94@gmail.com>
2023-01-16 17:10:44 -0300

    fix/techs

--- src/router/techs/techs.router.ts
+++ src/router/techs/techs.router.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express'
-import { createTechnologyController, deleteTechnolyController, listTechnologiesController } from '../../controllers/techs/techs.controller'
-import { ensuranceUserIsAdmMiddleware, ensurePatchDataIsValidMiddleware } from '../../middlewares'
-import { ensureAuthMiddleware } from '../../middlewares/ensureAuth.middleware'
-import { techSerializer } from '../../schemas/techs/technology.schema'
-
+import { listTechnologiesController } from '../../controllers/techs/techs.controller'
 
 const techsRoutes = Router()
 
-techsRoutes.post('', ensureAuthMiddleware, ensuranceUserIsAdmMiddleware, ensurePatchDataIsValidMiddleware(techSerializer), createTechnologyController)
 techsRoutes.get('', listTechnologiesController)
-techsRoutes.delete('/:id', ensureAuthMiddleware, ensuranceUserIsAdmMiddleware, deleteTechnolyController)
 
 export default techsRoutes
